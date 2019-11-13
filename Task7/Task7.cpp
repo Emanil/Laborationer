f8027997e2381aeea95a99bb7a08f70f5b4f4691
Emannil <emanil-9@student.ltu.se>
2019-11-13 11:26:38 +0100

    Finished task 7

--- Task7/Task7.cpp
+++ Task7/Task7.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-	
+	int n;
+	cout << "Number: ";
+	cin >> n;
+	if (n < 0) {
+		cout << "Error, negative number" << endl;
+	}
+	int n2 = n;
+	while (n > 0) {
+		cout << n << endl;
+		n--;	
+	}
+	while (n2 >= n) {
+		cout << n << endl;
+		n++;
+	}
+	return 0;
+			
 }
